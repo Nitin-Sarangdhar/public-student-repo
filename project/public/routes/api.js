00f75aad2ecec144386ddcc4d4c40bfd5ddd5f2e
Nitin-Sarangdhar <nitintechnology20@gmail.com>
2024-06-02 13:46:14 -0700

    Functional SocketIO code

--- project/public/routes/api.js
+++ project/public/routes/api.js
@@ -1,24 +1,33 @@
-// routes/api.js
 const express = require('express');
 const router = express.Router();
-const trialRoute = require('./trial');
-const trialPostRoute = require('./trialpost');
-// Import the update_all_cards route
-const updateAllCardsRoute = require('./update_all_cards');
 
-// Use the update_all_cards route
-router.use('/update_all_cards', updateAllCardsRoute);
+module.exports = function(io, app) {
+    // Middleware to pass io object
+    app.use((req, res, next) => {
+        req.io = io;
+        next();
+    });
 
-// Use the trial route
-router.use('/trial', trialRoute);
-router.use('/trialpost', trialPostRoute); // Include the trialpost route
+    const trialRoute = require('./trial');
+    const trialPostRoute = require('./trialpost');
+    // Import the update_all_cards route
+    const updateAllCardsRoute = require('./update_all_cards')(io);
 
-module.exports = router;
-console.log('API route initialized');
+    // Use the update_all_cards route
+    router.use('/update_all_cards', updateAllCardsRoute);
 
-// Example API route
-router.get('/data', (req, res) => {
-  console.log('Request received for /api/data');
-  // Your API logic here
-  res.json({ message: 'API data' });
-});
+    // Use the trial route
+    router.use('/trial', trialRoute);
+    router.use('/trialpost', trialPostRoute); // Include the trialpost route
+
+    console.log('API route initialized');
+
+    // Example API route
+    router.get('/data', (req, res) => {
+        console.log('Request received for /api/data');
+        // Your API logic here
+        res.json({ message: 'API data' });
+    });
+
+    return router; // Return the router instance
+};
