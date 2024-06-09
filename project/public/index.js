cb90b2f36752193bdb49557f45ad9140a540fae8
Nitin-Sarangdhar <nitintechnology20@gmail.com>
2024-06-08 19:04:47 -0700

    committing directly to public

--- project/public/index.js
+++ project/public/index.js
@@ -3,34 +3,35 @@ const path = require('path');
 const http = require('http');
 const socketIo = require('socket.io');
 const morgan = require('morgan');
+const cors = require('cors');
+const listEndpoints = require('express-list-endpoints');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
-const port = process.env.PORT || 3000;
-
-// Socket.IO event handling
-io.on('connection', (socket) => {
-    console.log('A client connected');
-
-    socket.on('disconnect', () => {
-        console.log('A client disconnected');
-    });
-    
-    // Handle other Socket.IO events as needed
+const io = socketIo(server, {
+    cors: {
+        origin: "*", // Allow all origins, or specify your frontend URL here
+        methods: ["GET", "POST"]
+    }
 });
+
+const port = process.env.PORT || 3000;
+console.log('PORT:', port);
+
+// Use the CORS middleware
+app.use(cors({
+    origin: '*', // Allow all origins
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type']
+}));
+
+app.use(express.urlencoded({ extended: false }));
 
 // Middleware to parse JSON
 app.use(express.json());
 
 // Use Morgan middleware for logging incoming requests
 app.use(morgan('dev'));
-
-// Import routes and pass io and app
-const apiRoutes = require('./routes/api')(io, app);
-
-// Use routes
-app.use('/api', apiRoutes);
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
@@ -39,6 +40,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/favicon.ico', (req, res) => {
     res.status(204).end();
 });
+
+// Socket.IO event handling
+io.on('connection', (socket) => {
+    console.log('A client connected');
+
+    socket.on('disconnect', () => {
+        console.log('A client disconnected');
+    });
+
+    // Handle other Socket.IO events as needed
+});
+
+// Import routes and pass io and app
+const apiRoutes = require('./routes/api')(io, app);
+
+// Use routes
+app.use('/api', apiRoutes);
 
 // Define a route to serve index.html
 app.get('/', (req, res) => {
@@ -68,14 +86,9 @@ app.get('/audio.mp3', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'audio.mp3'));
 });
 
-const listEndpoints = require('express-list-endpoints');
-
 // Print out all registered routes
 console.log(listEndpoints(app));
 
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
-
-
-
